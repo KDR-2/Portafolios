4ba7549ddfa7c3e62e7b5d0d619a800d97bde340
KDR-2 <keiberdrq22@gmail.com>
2025-01-26 19:29:31 -0400

    Primer commit de testing, modificaciones luego de meses sin codificar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,6 +5,6 @@ togglemenu.addEventListener("click", () => {
   navbar.classList.toggle("navigation--active");
 });
 
-particlesJS.load("particles-js", "/Portafolios/scripts/particles.json", function () {
+particlesJS.load("particles-js", "/scripts/particles.json", function () {
   console.log("callback - particles.js config loaded");
 });
